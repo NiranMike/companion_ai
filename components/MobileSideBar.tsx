a14b589fae617ba38f29c51fc6e651e25ebb6935
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-09-26 02:20:42 +0100

    fixed

--- components/MobileSideBar.tsx
+++ components/MobileSideBar.tsx
@@ -6,14 +6,15 @@ import {
     SheetTrigger
 } from "@/components/ui/sheet";
 import {SideBar} from "@/components/SideBar"
-const MobileSideBar = () => {
+const MobileSideBar = ({isPro}:{isPro : boolean}) => {
+   
   return (
     <Sheet>
         <SheetTrigger>
             <AiOutlineMenu size={25} className="block md:hidden" />
         </SheetTrigger>
         <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-            <SideBar />
+            <SideBar isPro={isPro} />
         </SheetContent>
     </Sheet>
   )
