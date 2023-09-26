9a06b3c7bb35b1724f03f1e07a7dcb32d80bf99e
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-09-26 11:04:24 +0100

    test

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -50,6 +50,7 @@ const DialogOverlay = React.forwardRef<
 ))
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
+
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
